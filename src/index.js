7d090163e768ee2b3d567429b787ccdc19bf12d0
yuanChuan <berserk5683@gmail.com>
2020-07-18 13:23:38 +0800

    threejs 绘制立方体

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Three from './pages/three';
 import Word from './pages/word';
+import Test from './pages/test';
 
 
 const Index = () => {
-  return <div><Three /></div>;
+  return <div><Test /></div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById('index'));
